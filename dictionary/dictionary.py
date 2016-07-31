44fe72eac68c621591e051f6a7d9524e5dbdb1ed
nbnbhattarai <070bct522@ioe.edu.np>
2016-08-01 02:18:21 +0545

    dictionary package created

---
+++ dictionary/dictionary.py
@@ -0,0 +1,16 @@
+import re
+
+
+class Dictionary:
+    """
+    Dictionary class which stores
+    all the words of a language
+    """
+    def __init__(self, lan='english'):
+        self.filename = './data/language/' + lan + '/dict'
+        self.file = open(self.filename, 'r')
+        if self.file:
+            text = self.file.read()
+            self.words = re.findall("[A-Za-z]+", text)
+        else:
+            self.words = None
