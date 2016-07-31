bb43c47b3315a8f554ad6ab17715404989aa5bd9
nbnbhattarai <070bct522@ioe.edu.np>
2016-07-31 19:27:30 +0545

    lemmatizer modified

--- lemmatizer/lemmatizer.py
+++ lemmatizer/lemmatizer.py
@@ -2,7 +2,7 @@
 Lemmatizer:
 -> Specially change symbol into predefined symbol like
 '.' -> '_FULLSTOP_'
-'?' -> '_QUESTIONMARK_' 
+'?' -> '_QUESTIONMARK_'
 '*' -> '_MULTIPLICATION_'
 '/' -> '_DIVISION_' etc.
 -> Change given numeral value into '_VARIABLE_#ID', where ID is replaced
@@ -12,17 +12,29 @@ for text ['how','is','10','*','30','?'], after lemmatization it will be
 ['how','is','_VARIABLE_#0','_MULTIPLICATION_','_VARIABLE_#1','_QUESTIONMARK_']
 variable are stored in actual _VARIABLE_ variable and accessed by the index
 
-We are changing mathematical symbol because we are going to use another 
-preprogrammed program for mathematical operations and return the result 
+We are changing mathematical symbol because we are going to use another
+preprogrammed program for mathematical operations and return the result
 back when these symbol are present because computer program are very fast
- compared to trained AI for mathematical operations.
+compared to trained AI for mathematical operations.
 """
 
+# english punctuation which are replaced in token list
+# by their meaningmul macros
 en_punc = {'.': '_FULLSTOP_',
            ',': '_COMMA_',
            '?': '_QUESTIONMARK_',
            '!': '_EXCLAMATIONMARK_',
+           ':': '_COLON_',
+           ';': '_SEMICOLON_',
+           '/': '_FORWARDSLASH_',
+           '\\': '_BACKWARDSLASH_',
+           '$': '_DOLLAR_',
+           '#': '_HASH_',
            }
+
+# math sumbols to be replaced in token list
+# containing these symbols in token perform mathematical
+# operations,...
 math_sym = {'+': '_PLUS_',
             '-': '_MINUS_',
             '*': '_MULTIPLICATION_',
@@ -31,6 +43,8 @@ math_sym = {'+': '_PLUS_',
             }
 
 
+# replace the meaningful symbols in word token
+# with special meaning symbol
 def lemmatize_word(word):
     """
     Lemmatize the given token and return lemmatized token.
@@ -39,13 +53,13 @@ def lemmatize_word(word):
         word = en_punc[word]
     elif word in math_sym.keys():
         word = math_sym[word]
-    
     return word
 
 
+# lemmatize all the words recursively in tokenlist
 def lemmatize_list(tokenlist):
     """
-    Lemmatize all token of tokenlist and return list containing 
+    Lemmatize all token of tokenlist and return list containing
     lemmatized tokens.
     """
     res = []
