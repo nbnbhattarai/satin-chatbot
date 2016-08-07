ab8fcab437ba900cacf9e679e0b38da339c5fe99
nbnbhattarai <070bct522@ioe.edu.np>
2016-08-07 23:34:37 +0545

    satin modified

--- satin.py
+++ satin.py
@@ -10,8 +10,8 @@ import languagemodel
 ngram = languagemodel.nGram()
 qgram = languagemodel.nGram()
 agram = languagemodel.nGram()
-qgram.trainFromFile('datalanguage/english/questions.txt')
-agram.trainFromFile('datalanguage/english/ans.txt')
+qgram.trainFromFile('data/language/english/questions.txt')
+agram.trainFromFile('data/language/english/ans.txt')
 ngram.trainFromFile('data/language/english/valveteen_rabbit.txt')
 tok = tokenizer.Tokenizer()
 
