cc38c2aceb4b980502eccc4a32413b9285eeb23d
nbn.bhattarai99@hotmail.com <070bct522@ioe.edu.np>
2016-08-15 19:57:42 +0545

    QAP class function and interpreat function modified

--- qap/qap.py
+++ qap/qap.py
@@ -1,4 +1,7 @@
-import worldmodel
+# Let's make predefined questions patterns. and with given answer to a
+# specific question pattern, let's update answer pattern.
+
+# import worldmodel
 import nltk
 
 questions_tx = ['who', 'what', 'where', 'how']
@@ -6,18 +9,31 @@ questions_tx = ['who', 'what', 'where', 'how']
 
 def interpreat_qa(q, a):
     """
-    q: questions' pos_tag tupples
-    a: answer's pos_tag tupples
-    replace name with respective symbols <obj1>, <obj2>, ...
+    q: questions' token list
+    a: answer's token list
+    replace name with respective symbols <obj0>, <obj1>, ...
+    <obj> are nouns in question and same noun in answer are replaced
+    with respective <objx> symbol.
+
+    example:
+    q = ['what', 'is', 'a', 'cat', '?']
+    a = ['a', 'cat', 'is', 'a', 'animal', '.']
+
+    the result will be
+    pq_res = 'what is a <obj0> ?'
+    pa_res = 'a <obj0> is a animal .'
+    objs = ['cat']
     """
+
     pq = nltk.pos_tag(q, tagset='universal')
     pa = nltk.pos_tag(a, tagset='universal')
+
     i = 0
-    obj_q = []
-    pq_res = []
-    pa_res = []
-    ques = []
-    answers = []
+    objs = []              # list of objects in question/answer
+    pq_res = []                 # final result of xml syntax question
+    pa_res = []                 # final result of xml syntax answer
+    ques = []                   # find question types list from question_tx
+
     for k in questions_tx:
         if k in q:
             ques.append(k)
@@ -25,37 +41,19 @@ def interpreat_qa(q, a):
     for a in pq:
         if a[1] == 'NOUN':
             pq_res.append('<obj' + str(i) + '>')
-            obj_q.append(a[0])
+            objs.append(a[0])
             i = i + 1
         else:
             pq_res.append(a[0] + ' ')
-    whatclass = ''
+
     for i in range(len(pa)):
         a = pa[i]
-        if a[0] == 'PRON':
-            if len(a) > i + 1:
-                if pa[i + 1][0] == 'NOUN':
-                    whatclass = ''.join(a[0])
-                else:
-                    pa_res.append(a[0])
-        if a[0] in obj_q:
-            j = obj_q.index(a[0])
+        if a[0] in objs:
+            j = objs.index(a[0])
             pa_res.append('<obj' + str(j) + '>')
-        if a[1] == 'VERB':
-            pa_res.append(a[0])
-            answers = pa[i + 1:-1]
-            j = 0
-            for oq in obj_q:
-                if j > 0:
-                    pa_res.append('and')
-                pa_res.append('<obj' + str(j) + '.' + ques[0] + '>')
-                j = j + 1
-
-            pa_res.append(pa[-1][0])
-            break
         else:
             pa_res.append(a[0])
-    return [pq_res, pa_res, answers]
+    return [pq_res, pa_res, objs]
 
 
 class QAP:
@@ -71,8 +69,31 @@ class QAP:
         self.worldmodel = wmodel
 
     def train_from_tokens(self, qt, at):
-        [q_st, a_st] = interpreat_qa(qt, at)
-        self.qas.append((q_st, a_st))
+        [q_st, a_st, objs] = interpreat_qa(qt, at)
+        # add tuple in database with first two question and answer
+        # pattern and last one is object name to which the pattern
+        # can be used.
+        self.qas.append((q_st, a_st, objs))
+
+    def get_answer_from_question(self, q):
+        pass
 
     def load_from_file(self, filename):
+        """
+        Get patterns and objects from a xml type file.
+        example:
+        <qa></qa> : inside it, question/answer pattern are saved.
+        <q></q>   : question pattern with <objx> tags.
+        <objx>    : object which is replaced in answer with actual
+        objects.
+        <a></a>   : inside it, there will be a pattern for answer
+        which contains <objx> to be replaced by actual object to
+        form an answer.
+        <aa></aa> : it contains a alternate answer pattern, which is used
+        if no answer pattern is appropriate.
+        <o></o>   : it contains name of objets with comma seperated format,
+        that answer pattern is used for those specific objects. if all objects
+        from a specific class exist, then a class name is added insted of
+        object name.
+        """
         pass
